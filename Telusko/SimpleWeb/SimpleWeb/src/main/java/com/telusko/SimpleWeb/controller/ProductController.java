1aa3dc3709fc82f326e02bf7cd9697be2938b66b
Suvan77blr <sbumsrit61blr@gmail.com>
2025-12-08 01:09:31 +0530

    Telusko: Implemented @PutMapping, @DeleteMapping => with static products List ( ArrayList<> )
    Signed-off-by: Suvan77blr <sbumsrit61blr@gmail.com>

--- Telusko/SimpleWeb/SimpleWeb/src/main/java/com/telusko/SimpleWeb/controller/ProductController.java
+++ Telusko/SimpleWeb/SimpleWeb/src/main/java/com/telusko/SimpleWeb/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.telusko.SimpleWeb.controller;
 
 import com.telusko.SimpleWeb.model.Product;
 import com.telusko.SimpleWeb.service.ProductService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,10 +25,23 @@ public class ProductController {
         return productService.getProductById(prodId);
     }
 
-    @PostMapping("/product")
+    @PostMapping("/products")
     public String addProduct(@RequestBody Product product) {
         System.out.println(product);
         return productService.addProduct(product);
     }
 
+    @PutMapping("/products")
+    public String updateProduct(@RequestBody Product product) {
+        int result = productService.updateProduct(product);
+        return (result == 0) ? "Update successful" : "Update unsuccessful";
+    }
+
+//    @DeleteMapping(value = "/product", params = "{prodId}")
+    @DeleteMapping("/products/{prodId}")
+    public String deleteProduct(@PathVariable int prodId) {
+//    public String deleteProduct(@RequestParam int prodId) {
+        int result = productService.deleteProduct(prodId);
+        return (result == 0) ? "Delete successful" : "Delete unsuccessful";
+    }
 }
